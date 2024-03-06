3d61146b7c532dfe3c1d311f4707626859093ee8
SabehShaikh <sabehshaikh201@gmail.com>
2024-03-06 23:29:44 +0500

    29 exercises done.

--- TypeScript/AssignmentQuestions(25-45)/app.js
+++ TypeScript/AssignmentQuestions(25-45)/app.js
@@ -28,3 +28,92 @@ if (alien_color == 'green') {
 else {
     console.log(`Player just earned 10 points.`);
 }
+// 27-Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+// • Write three versions of this program, making sure each message is printed for the appropriate color alien.
+// • If the alien is green, print a message that the player earned 5 points.
+let aliencolor = 'green';
+if (aliencolor == 'green') {
+    console.log(`Player earned 5 points.`);
+}
+else if (aliencolor == 'yellow') {
+    console.log(`Player earned 10 points.`);
+}
+else if (aliencolor == 'red') {
+    console.log(`Player earned 15 points.`);
+}
+else {
+    console.log(`wrong color selected.`);
+}
+// version two: 
+// • If the alien is red, print a message that the player earned 15 points.
+aliencolor = 'red';
+if (aliencolor == 'green') {
+    console.log(`Player earned 5 points.`);
+}
+else if (aliencolor == 'yellow') {
+    console.log(`Player earned 10 points.`);
+}
+else if (aliencolor == 'red') {
+    console.log(`Player earned 15 points.`);
+}
+// version three:
+// • If the alien is yellow, print a message that the player earned 10 points.
+aliencolor = 'yellow';
+if (aliencolor == 'green') {
+    console.log(`Player earned 5 points.`);
+}
+else if (aliencolor == 'yellow') {
+    console.log(`Player earned 10 points.`);
+}
+else if (aliencolor == 'red') {
+    console.log(`Player earned 15 points.`);
+}
+// 28- Stages of Life: Write an if-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+let age = 20;
+// • If the person is less than 2 years old, print a message that the person is a baby.
+if (age < 2) {
+    console.log(`person is a baby.`);
+}
+// • If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+else if (age >= 2 && age < 4) {
+    console.log(`person is a toddler.`);
+}
+// • If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+else if (age >= 4 && age < 13) {
+    console.log(`person is a kid.`);
+}
+// • If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+else if (age >= 13 && age < 20) {
+    console.log(`person is a teenager.`);
+}
+// • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+else if (age >= 20 && age < 65) {
+    console.log(`person is an adult.`);
+}
+// • If the person is age 65 or older, print a message that the person is an elder.
+// else {
+//     console.log(`person is an elder.`)
+// }
+// Another way
+else if (age >= 65) {
+    console.log(`person is an elder.`);
+}
+// 29-Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+// • Make a array of your three favorite fruits and call it favorite_fruits.
+let favorite_fruits = ['mango', 'orange', 'strawberry'];
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+if (favorite_fruits.includes('mango')) {
+    console.log(`I really like mango!`);
+}
+if (favorite_fruits.includes('orange')) {
+    console.log(`I really like orange!`);
+}
+if (favorite_fruits.includes('strawberry')) {
+    console.log(`I really like strawberry!`);
+}
+if (favorite_fruits.includes('banana')) {
+    console.log(`I really like banana!`);
+}
+if (favorite_fruits.includes('kiwi')) {
+    console.log(`I really like kiwi`);
+}
