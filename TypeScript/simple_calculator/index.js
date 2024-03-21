c5db9b2f3a0653e8cfdaf1c360deff559268b15b
SabehShaikh <sabehshaikh201@gmail.com>
2024-03-21 23:05:34 +0500

    add conditional statements.

--- TypeScript/simple_calculator/index.js
+++ TypeScript/simple_calculator/index.js
@@ -17,4 +17,19 @@ const answer = await inquirer.prompt([
         choices: ["Addition", "Subtraction", "Multiplication", "Division"],
     },
 ]);
-console.log(answer);
+// conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Please select valid operator");
+}
