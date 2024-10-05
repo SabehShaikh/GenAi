2f63f611b8893260ccf9f7bf7b3e6ee3256a9c12
SabehShaikh <sabehshaikh201@gmail.com>
2024-10-05 23:57:18 +0500

    add class05 code.

---
+++ NextJS/ClassCodes/class05/src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../globals.css";
+const page = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  );
+};
+
+export default page;
---
+++ NextJS/ClassCodes/class05/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className=" bg-blue-500">
+      <h1>Footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
