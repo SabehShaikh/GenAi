026ea80d7e65d9950f4179312ac7da608421f6bf
SabehShaikh <sabehshaikh201@gmail.com>
2024-12-26 21:06:09 +0500

    add class13 code, how to render img from sanity.

--- NextJS/ClassCodes/class12/next.config.mjs
+++ NextJS/ClassCodes/class12/next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ["cdn.sanity.io"], // Allow Sanity's image CDN
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
--- NextJS/ClassCodes/class12/src/app/page.tsx
+++ NextJS/ClassCodes/class12/src/app/page.tsx
@@ -1,13 +1,16 @@
 import { client } from "@/sanity/lib/client";
 import Image from "next/image";
+import { urlFor } from "@/sanity/lib/image";
 
 export default async function Home() {
   const query = `*[_type == "homepage"] {
     heading,
     paragraph,
     buttonText,
-    "imageUrl": image.asset->url
+    image,
   }`;
+
+  // "imageUrl": image.asset->url
 
   const sanityData = await client.fetch(query);
   const homepage = sanityData[0];
@@ -15,21 +18,18 @@ export default async function Home() {
   return (
     <div className="p-4">
       <h1 className="font-bold text-2xl mb-4">{homepage.heading}</h1>
+
+      <Image
+        src={urlFor(homepage.image).url()}
+        alt=""
+        width={300}
+        height={300}
+      />
+
       <p className="text-lg mb-4">{homepage.paragraph}</p>
       <button className="bg-blue-500 text-white px-4 py-2 rounded">
         {homepage.buttonText}
       </button>
-
-      {/* if homepage.imageUrl is available. */}
-      {homepage.imageUrl && (
-        <Image
-          src={homepage.imageUrl}
-          alt="Homepage"
-          width={300}
-          height={250}
-          className="mt-4 rounded shadow-lg"
-        />
-      )}
     </div>
   );
 }
